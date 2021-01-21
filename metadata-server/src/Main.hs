48f06f03e77623fa312c4d220c6b78801a16ca4c
Samuel Evans-Powell <mail@sevanspowell.net>
2021-01-21 16:00:06 +0800

    WIP

--- metadata-server/src/Main.hs
+++ metadata-server/src/Main.hs
@@ -1,3 +1,87 @@
+module Main where
+
+import Data.Text (Text)
+
+type Subject = Text
+
+type MetadataServer =
+  "metadata" :>
+    ( Capture "subject" Subject                                                    :> Get '[JSON] MetadataEntry
+ :<|> Capture "subject" Subject :> "properties"                                    :> Get '[JSON] MetadataEntry
+ :<|> Capture "subject" Subject :> "properties" :> Capture "properties" [Property] :> Get '[JSON] AnyMetadataProperty
+ :<|> "query" :> ReqBody '[JSON] BatchRequest :> Post '[JSON] BatchResponse
+    )
+
+-- TODO Take "write" and "read" functions as arguments to a function, expose that function
+-- Allows users of this library to easily choose their DB backend
+-- We'll use psql database backend here.
+
+-- TODO Ensure file size < 400kb
 
 main :: IO ()
 main = putStrLn "Hello metadata-server"
+
+-- | More loosely-typed metadata property useful in looser typed contexts.
+-- For exa
+type AnyMetadataProperty = PropertyPreImage PreImage
+                         | PropertySubject  Subject
+                         | PropertyOwner    Owner
+                         | PropertyGeneric  MetadataProperty
+
+-- More loosely-typed metadata entry useful in looser typed contexts
+data AnyMetadataEntry
+  = AnyMetadataEntry { anyMeSubject    :: Subject
+                     , anyMeProperties :: [AnyMetadataProperty]
+                     }
+
+data BatchRequest
+  = BatchRequest { bReqSubjects      :: [Subject]
+                 , bReqPropertyNames :: [Text]
+                 }
+
+data BatchResponse
+  = BatchResponse { bRespSubjects :: [AnyMetadataEntry] }
+
+ 
+data MetadataEntry
+  = MetadataEntry { mePreImage    :: PreImage
+                  , meSubject     :: Subject
+                  , meDescription :: MetadataProperty
+                  , meName        :: MetadataProperty
+                  , meOwner       :: Owner
+                  }
+
+data PreImage
+  = PreImage { piValue  :: Text
+             , piHashFn :: HashFn
+             }
+
+data Owner
+  = Owner { ownPublicKey :: Text
+          , ownSignature :: Text
+          }
+
+data MetadataProperty
+  = MetadataProperty { mpValue        :: Text
+                     , mpAnSignatures :: [AnnotatedSignature]
+                     }
+
+data AnnotatedSgnature =
+  AnnotatedSgnature { asPublicKey :: Text
+                    , asSignature :: Text
+                    }
+
+data HashFn = Blake2b256
+            | Blake2b224
+            | SHA256
+
+instance Show HashFn where
+  show Blake2b256 = "blake2b-256"
+  show Blake2b224 = "blake2b-224"
+  show SHA256     = "sha256"
+
+instance Read HashFn where
+  readPrec = Read.lift $ choice [ pure Blake2b256 <$ string "blake2b-256"
+                                , pure Black2b224 <$ string "blake2b-224"
+                                , pure SHA256     <$ string "sha256"
+                                ]
--- metadata-webhook/src/Main.hs
+++ metadata-webhook/src/Main.hs
@@ -1,3 +1,58 @@
+module Main where
+
+import           Control.Monad.IO.Class       ( liftIO )
+import qualified Data.ByteString              as BS
+import qualified Data.ByteString.Char8        as C8
+import           Data.Maybe                   ( fromJust )
+import           GitHub.Data.Webhooks.Events  ( PushEvent(..), IssueCommentEvent(..) )
+import           GitHub.Data.Webhooks.Payload ( HookIssueComment(..), HookUser(..) )
+import           Network.Wai                  ( Application )
+import           Network.Wai.Handler.Warp     ( run )
+import           System.Environment           ( lookupEnv )
+import qualified Data.Aeson as Aeson
+
+-- Using servant and servant-github-webhook to serve the API
+import           Servant
+import qualified Servant.GitHub.Webhook       as SGH
+import           Servant.GitHub.Webhook       ( GitHubEvent, GitHubSignedReqBody, RepoWebhookEvent(..) )
+
+-- Lib
+newtype GitHubKey = GitHubKey (forall result. SGH.GitHubKey result)
+
+gitHubKey :: IO BS.ByteString -> GitHubKey
+gitHubKey k = GitHubKey (SGH.gitHubKey k)
+
+instance HasContextEntry '[GitHubKey] (SGH.GitHubKey result) where
+    getContextEntry (GitHubKey x :. _) = x
+
+-- API defn.
+type MetadataWebhookAPI = "webhook" :> PushHookAPI
+
+type PushHookAPI
+  =  GitHubEvent '[ 'WebhookPushEvent ]
+  :> GitHubSignedReqBody '[JSON] Aeson.Object
+  :> Post '[JSON] ()
+
+-- API handlers
+pushHook :: RepoWebhookEvent -> ((), Aeson.Object) -> Handler ()
+pushHook _ (_, ev) = liftIO $ do
+    putStrLn $ (show . whUserLogin . evPushSender) ev ++ " pushed a commit, resulting in the following event: "
+    putStrLn $ show ev
+
+metadataWebhook :: Server MetadataWebhookAPI
+metadataWebhook = pushHook
 
 main :: IO ()
-main = putStrLn "Hello metadata-webhook"
+main = do
+    port <- maybe 8080 read <$> lookupEnv "PORT"
+    key <- maybe mempty C8.pack <$> lookupEnv "KEY"
+    putStrLn $ "Server is starting on port " ++ show port ++ " using test secret " ++ show key
+    putStrLn $ "Perhaps run 'ngrok http " ++ show port ++ "' for a forwarding address"
+    run port (app (gitHubKey $ pure key))
+
+app :: GitHubKey -> Application
+app key
+  = serveWithContext
+    (Proxy :: Proxy MetadataWebhookAPI)
+    (key :. EmptyContext)
+    singleEndpoint
