34a8bfb40310cbb0aad4d9ce09ad9524674dcd1a
Samuel Evans-Powell <mail@sevanspowell.net>
2021-02-01 17:38:19 +0800

    Remove bad defaults

--- metadata-server/src/Config.hs
+++ metadata-server/src/Config.hs
@@ -17,9 +17,9 @@ data Opts = Opts
 
 parseOpts :: Parser Opts
 parseOpts = Opts
-  <$> strOption (long "db" <> metavar "DB_NAME" <> showDefault <> value "cexplorer" <> help "Name of the database to store and read metadata from")
-  <*> strOption (long "db-user" <> metavar "DB_USER" <> showDefault <> value "cexplorer" <> help "User to connect to metadata database with")
-  <*> strOption (long "db-host" <> metavar "DB_HOST" <> showDefault <> value "/run/postgresql" <> help "Host for the metadata database connection")
+  <$> strOption (long "db" <> metavar "DB_NAME" <> help "Name of the database to store and read metadata from")
+  <*> strOption (long "db-user" <> metavar "DB_USER" <> help "User to connect to metadata database with")
+  <*> strOption (long "db-host" <> metavar "DB_HOST" <> help "Host for the metadata database connection")
   <*> strOption (long "db-table" <> metavar "DB_TABLE" <> showDefault <> value "metadata" <> help "Table in the database to store metadata")
   <*> option auto (long "db-conns" <> metavar "INT" <> showDefault <> value 1 <> help "Number of connections to open to the database")
   <*> option auto (short 'p' <> long "port" <> metavar "PORT" <> showDefault <> value 8080 <> help "Port to run the metadata web server on")
