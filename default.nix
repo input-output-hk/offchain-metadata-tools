ab05bff2547092369543269b29c40364d6d15711
IOHK <devops+nix-tools@iohk.io>
2021-04-14 05:37:26 +0000

    Update gh-pages for 4b97265

---
+++ default.nix
@@ -0,0 +1,24 @@
+{ emacs, git, cardano-cli, cardano-metadata-submitter, runCommand, lib, coreutils, gawk, curl, jq }:
+
+let
+  buildInputs =
+    [ emacs
+      git
+      coreutils
+      gawk
+      curl
+    ];
+in
+  runCommand
+    "generate-documentation-scripts"
+    { inherit buildInputs; }
+    ''
+      mkdir -p $out/bin
+      cd $out/bin
+
+      cp ${./index.org} ./index.org
+      emacs --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "./index.org")'
+      mv ./index.org $out/index.org
+      bash ./mk-nix-setup.sh
+      mv metadata-config.nix $out/
+    ''
